ecf93aeda3f1a857db400256119da46739dd0833
swathi manikyam <saiswathidulipudi@gmail.com>
2023-04-14 17:37:06 +0530

    added div section

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
-import { useNavigationType, useLocation } from 'react-router-dom';
-import NavBar from './pages/navbar';
-import { useEffect } from 'react';
+/** @format */
+
+import { useNavigationType, useLocation } from "react-router-dom";
+import NavBar from "./pages/navbar";
+import { useEffect } from "react";
 
 function App() {
-    const action = useNavigationType();
-    const location = useLocation();
-    const pathname = location.pathname;
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
 
-    useEffect(() => {
-        if (action !== 'POP') {
-            window.scrollTo(0, 0);
-        }
-    }, [action, pathname]);
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
 
-    useEffect(() => {
-        let title = '';
-        let metaDescription = '';
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
 
-        // eslint-disable-next-line default-case
-        switch (pathname) {
-            case '/':
-                title = '';
-                metaDescription = '';
-                break;
-        }
+    // eslint-disable-next-line default-case
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+    }
 
-        if (title) {
-            document.title = title;
-        }
+    if (title) {
+      document.title = title;
+    }
 
-        if (metaDescription) {
-            const metaDescriptionTag = document.querySelector(
-                'head > meta[name="description"]'
-            );
-            if (metaDescriptionTag) {
-                metaDescriptionTag.content = metaDescription;
-            }
-        }
-    }, [pathname]);
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
 
-    return <NavBar />;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 export default App;
